82445fcfa0331c26cc6655a1cb2e831db05cfe5d
Yugo Miyahara <yugo.miyahara@taqtile.com.br>
2020-05-16 00:30:52 -0300

    create redirect path

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import { Login } from "./components/login";
 import { Home } from "./components/home";
 
@@ -10,10 +10,26 @@ function App() {
             <Route path="/login">
                 <Login />
             </Route>
+            <Route>
+                <AuthRoutes/>
+            </Route>
+        </Switch>
+    );
+}
+
+const AuthRoutes = () => {
+    if(!window.localStorage.getItem('token')){
+        return <Redirect to="/login"/>
+    }
+    return (
+        <>
+            <Route path="/">
+                <Redirect to="/home"/>
+            </Route>
             <Route path="/home">
                 <Home />
             </Route>
-        </Switch>
+        </>
     );
 }
 
